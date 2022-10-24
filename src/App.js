671efd1b1831c71c1e3c1802da62c9b1bc2cb5ba
Leonardo Kepler Mesquita <leokepler@gmail.com>
2022-10-24 16:24:20 -0300

    Rotas :white_check_mark:

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import MyProvider from './context/myProvider';
 import Login from './page/Login';
+import Meals from './page/Meals';
+import Drinks from './page/Drinks';
+import Profile from './page/Profile';
+import DoneRecipes from './page/DoneRecipes';
 
 function App() {
   return (
@@ -10,6 +14,11 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ Login } />
+          <Route exact path="/meals" component={ Meals } />
+          <Route exact path="/drinks" component={ Drinks } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/done-recipes" component={ DoneRecipes } />
+          <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
         </Switch>
       </BrowserRouter>
     </MyProvider>
