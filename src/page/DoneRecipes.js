3d727063068dfeb88b948109bfc6c2f9e2d71b2d
Leonardo Kepler Mesquita <leokepler@gmail.com>
2022-10-31 16:22:52 -0300

    Requisito 44 :white_check_mark:

--- src/page/DoneRecipes.js
+++ src/page/DoneRecipes.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../component/Header';
 
 function DoneRecipes() {
+  const [storage, setStorage] = useState([]);
+
+  useEffect(() => {
+    const storageLocal = JSON.parse(localStorage.getItem('doneRecipes'));
+    if (storageLocal !== null) {
+      setStorage(storageLocal);
+    }
+  }, []);
+
   return (
     <div>
       <Header title="Done Recipes" />
+      <button type="button" data-testid="filter-by-all-btn">All</button>
+      <button type="button" data-testid="filter-by-meal-btn">Meals</button>
+      <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
+      {storage.map((recipe, index) => (
+        <div key={ recipe.id }>
+          <img
+            data-testid={ `${index}-horizontal-image` }
+            src={ recipe.image }
+            alt={ recipe.name }
+          />
+          <span data-testid={ `${index}-horizontal-top-text` }>{recipe.category}</span>
+          <span data-testid={ `${index}-horizontal-name` }>{recipe.name}</span>
+          <span data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</span>
+          <button
+            type="button"
+            data-testid={ `${index}-horizontal-share-btn` }
+          >
+            Share
+          </button>
+          {recipe.tags !== null && (
+            recipe.tags.map((tag) => (
+              <span
+                key={ tag }
+                data-testid={ `${index}-${tag}-horizontal-tag` }
+              >
+                {tag}
+              </span>
+            ))
+          )}
+        </div>
+      ))}
     </div>
   );
 }
